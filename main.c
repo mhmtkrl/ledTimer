f5b438571d3188988d63022ecf22212cd84b5895
Mehmet KORAL <mehmet.koral96@gmail.com>
2019-10-30 22:55:44 +0300

    led blink with user button

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include "stm32f4xx.h"                  						// Device header
 
+#define	UserButton	(GPIOA->IDR & 0x01)							//User Button -> PA0
+
 void delayMs(uint32_t delay) {											//Delay function
 	while(delay > 0) delay--;
 }
 
 int main() {
+	RCC->AHB1ENR |= 1ul << 0;													//Clock signal for GPIOA
 	RCC->AHB1ENR |= 1ul << 3;													//Clock signal for GPIOD
 	
 	GPIOD->MODER = 0x55000000;												//PD12 - PD15 -> Output
 	
 	while(1) {
-		GPIOD->ODR |= 1ul << 12;
-		delayMs(1000000);
-		GPIOD->ODR &= ~(1ul << 12);
-		delayMs(1000000);
+		if(UserButton) {
+			delayMs(10000);
+			GPIOD->ODR |= 1ul << 14;
+		}
+		else {
+			GPIOD->ODR &= ~(1ul << 14);
+		}
 	}
 }
